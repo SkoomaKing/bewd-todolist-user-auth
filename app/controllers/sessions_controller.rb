f0b117899aea7dd1e567a9a6ea3d920eed1664df
Caleb Burrus <calebburrus@gmail.com>
2023-12-21 16:10:35 -0500

    sessions controller create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,22 @@
 class SessionsController < ApplicationController
-  def create
+  def create # rubocop:disable Metrics/AbcSize
+    @user = User.find_by(username: params[:user][:username])
+
+    if @user && (@user.password == params[:user][:password])
+      session = @user.sessions.create
+      cookies.permanent.signed[:todolist_session_token] = {
+        value: session.token,
+        httponly: true
+      }
+
+      render json: {
+        success: true
+      }
+    else
+      render json: {
+        success: false
+      }
+    end
   end
 
   def authenticated
